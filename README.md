d5e1619d08bc9f5787f92795397c912cc7244694
grizz-bot92 <bbenoit752@gmail.com>
2026-04-05 08:25:23 -0700

    fully deployed app using fly

---
+++ README.md
@@ -0,0 +1,1 @@
+-- https://endless-dnf.fly.dev/
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', req => JSON.stringify(req.body))
